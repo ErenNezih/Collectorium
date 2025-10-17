44b9f52c593258daa0be5ec0865d827acabf2ea0
Eren Nezih <erenezihnberber1@gmail.com>
2025-10-17 03:54:04 +0300

    fix(search): make 0002 self-contained initial migration; drop nonexistent 0001 dependency

--- search/migrations/0002_savedsearch_pricepoint.py
+++ search/migrations/0002_savedsearch_pricepoint.py
@@ -5,8 +5,9 @@ from django.conf import settings
 
 class Migration(migrations.Migration):
 
+    initial = True
+
     dependencies = [
-        ("search", "0001_initial"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ("listings", "0004_listing_location_indexes"),
     ]
